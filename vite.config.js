66e543c14a6bca71c45c521c96e0df935a4d5344
imperialking123 <jacobmessiah71@gmail.com>
2025-09-11 13:14:17 +0100

    fifth commit

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    historyApiFallback: true, // 👈 ensures dev server behaves like Vercel
+  },
+  build: {
+    outDir: 'dist', // 👈 make sure this matches what Vercel expects
+  },
 })
